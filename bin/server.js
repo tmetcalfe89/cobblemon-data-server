a204be1306f72801e6d62dbe2a372b46c195c34a
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-01-03 01:25:38 -0800

    cors

--- bin/server.js
+++ bin/server.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("../routes");
+const cors = require("cors");
 
 const app = express();
 
 const { MONGO_URI, PORT = 3000 } = process.env;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);

9652ecc458ac4eb530caec035c282fa1d82742f0
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-02-12 10:09:45 -0800

    added search api

--- controllers/spawns.js
+++ controllers/spawns.js
@@ -11,5 +11,50 @@ module.exports = {
       }
     });
     res.json(results);
+  },
+  search: async (req, res) => {
+    const pageSize = 20;
+    const { page = 0 } = req.query;
+    const { filter = {}, include = [] } = req.body || {};
+    const results = await SpawnModel.aggregate(
+      [
+        {
+          $lookup: {
+            as: 'species',
+            from: 'species',
+            foreignField: 'sname',
+            localField: 'sname'
+          }
+        },
+        {
+          $unwind: {
+            path: "$species",
+          }
+        },
+        {
+          $addFields: {
+            "species.drops": {
+              $ifNull: ["$drops", "$species.drops"]
+            }
+          }
+        },
+        { $match: filter },
+        {
+          $sort: {
+            sname: 1
+          }
+        },
+        { $skip: page * pageSize },
+        { $limit: pageSize },
+        {
+          $project: {
+            pokemon: 1,
+            ...include.reduce((acc, k) => ({ ...acc, [k]: 1 }), {})
+          }
+        }
+      ],
+      { maxTimeMS: 60000, allowDiskUse: true }
+    );
+    res.json(results);
   }
 }
--- routes/api/v1/spawns/index.js
+++ routes/api/v1/spawns/index.js
@@ -4,5 +4,6 @@ const spawnsController = require("../../../../controllers/spawns")
 const router = express.Router();
 
 router.get("/:name", spawnsController.getAllFor);
+router.post("/search", spawnsController.search);
 
 module.exports = router;
